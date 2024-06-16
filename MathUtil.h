7ff7077091b2d8a72067672270d31455480c7891
Lennie De Villiers <lennie.work@gmail.com>
2024-06-16 11:48:52 +0200

    Update MathUtil.h

--- MathUtil.h
+++ MathUtil.h
@@ -1,7 +1,11 @@
 #pragma once
 
+/*
+  Math functions that isn't part of the C++ Standard Library
+*/
 class MathUtil
 {
 public:
+    // Calculate the factorial of a number
     static long factorial(const int n);
 };
