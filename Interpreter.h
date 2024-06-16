a8c2f8a9049ab131ab2fe6ff3429eadd0ebf1415
Lennie De Villiers <lennie.work@gmail.com>
2024-06-16 11:52:17 +0200

    Update Interpreter.h

--- Interpreter.h
+++ Interpreter.h
@@ -7,18 +7,25 @@
 
 class Interpreter
 {
-
 public:
+    // Constructor
     Interpreter(const Stack &stack, const SymbolTable &table) : myStack(stack), table(table) {}
 
+	// Compute the RPN expression
     double* compute(const std::string &expression);
 
 private:
+    // Reference to the Stack
     Stack myStack;
+    // Reference to the Symbol Table
     SymbolTable table;
+    // Indicate if char is a operator
     bool IsOperator(char);
+    // Get the value from the token being number of value of a veriable from symbol table
     double getValue(char);
-    bool isVariable(char);
+    // is a variable
+    bool isValidVariable(char);
+    // Error handling, check for divison by zero and operants requried
     void checkDivByZero(double);
     void checkRequireOperants(int);
 };
