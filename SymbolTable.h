ae0a4aa029a24351e901e1a060f2531514b620e3
Lennie De Villiers <lennie.work@gmail.com>
2024-06-16 11:38:45 +0200

    Update SymbolTable.h

--- SymbolTable.h
+++ SymbolTable.h
@@ -1,22 +1,35 @@
 #pragma once
 
-struct Symbol
-{
-    char name;
-    double value;
-};
+#include 'config.h'
 
 class SymbolTable
 {
 public:
-    SymbolTable();
+    // Constructor 
+    SymbolTable(); 
 
+	// Set variable value
     void setValue(char name, double value);
+    
+    // Get value of a variable 
     double getValue(char name);
+    
+    // Print content
     void printTable();
+    
+    // Is a variable and not a digit
     bool isVariable(char);
+    
+    // Is a valid variable in the range A-Z
     bool isValid(char);
 
 private:
-    Symbol variables[26];
+    // Structure of each variable element
+	struct Symbol
+	    char name;
+ 	   double value;
+	};
+
+    // Array of variables A-Z
+    Symbol variables[SYMBOL_TABLE_SIZE];
 };
