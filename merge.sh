b8438ea0cb5dd9b4fcac953d67e3642da0c929b0
Lennie De Villiers <lennie.work@gmail.com>
2024-06-18 07:29:33 +0200

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,4 +1,4 @@
-for file in $(ls *.cpp *.h | sort); do
+for file in $(ls *.h *.cpp | sort); do
   echo "// File: $file" >> combined.txt
   cat "$file" >> combined.txt
   echo "" >> combined.txt  # Add a newline for separation
